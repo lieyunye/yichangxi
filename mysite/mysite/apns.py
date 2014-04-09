99d1c8d3ca7c275753d8df1d13559c30c17bbfda
majin <lieyunye@gmail.com>
2014-04-09 10:51:00 +0800

    final push

--- mysite/mysite/apns.py
+++ mysite/mysite/apns.py
@@ -14,14 +14,6 @@ def Payload(alert='', badge=1, data={}):
 
 def APN(token, payload, theCertfile):
     theHost = ( 'gateway.sandbox.push.apple.com', 2195 )
-    data = json.dumps( payload )
-    # Clear out spaces in the device token and convert to hex
-    deviceToken = token.replace(' ','').decode('hex')
-    byteToken = binascii.unhexlify(token)
-
-    theFormat = '!BH32sH%ds' % len(data)
-    theNotification = struct.pack( theFormat, 0, 32, byteToken, len(data), data )
-
     # Create our connection using the certfile saved locally
     ssl_sock = ssl.wrap_socket(
             socket.socket( socket.AF_INET, socket.SOCK_STREAM ),
@@ -30,7 +22,16 @@ def APN(token, payload, theCertfile):
     ssl_sock.connect( theHost )
 
     # Write out our data
-    ssl_sock.write( theNotification )
+    for x in xrange(1,150):
+        payload['aps']['alert'] = str(x)
+        data = json.dumps( payload )
+        # Clear out spaces in the device token and convert to hex
+        deviceToken = token.replace(' ','').decode('hex')
+        byteToken = binascii.unhexlify(token)
+        theFormat = '!BH32sH%ds' % len(data)
+        theNotification = struct.pack( theFormat, 0, 32, byteToken, len(data), data )
+        print x
+        ssl_sock.write( theNotification )
 
     # Close the connection -- apple would prefer that we keep
     # a connection open and push data as needed.
